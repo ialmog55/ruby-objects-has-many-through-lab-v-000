1e14244c19a30e5c5eeaa059c1c599cc5153190a
idith almog <almog.idith@gmail.com>
2020-05-28 18:28:19 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,10 @@
+class Appointment
+  attr_accessor :date, :patient, :doctor
+  @@all=[]
+  def initialize(date,patient,doctor)
+    @patient=patient
+    @date=date
+    @doctor=doctor
+    @@all << self
+  end
+end
